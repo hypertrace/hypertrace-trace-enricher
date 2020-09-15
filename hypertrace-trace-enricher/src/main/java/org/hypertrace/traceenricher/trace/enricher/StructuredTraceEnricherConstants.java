af9fba6072d7c35ad153304605b5dff4fa5b7bee
Ronak <ronak@traceable.ai>
2020-09-15 18:01:44 +0530

    incorporate the latest kafka-stream-framework related changes

--- hypertrace-trace-enricher/src/main/java/org/hypertrace/traceenricher/trace/enricher/StructuredTraceEnricherConstants.java
+++ hypertrace-trace-enricher/src/main/java/org/hypertrace/traceenricher/trace/enricher/StructuredTraceEnricherConstants.java
@@ -1,12 +1,12 @@
 package org.hypertrace.traceenricher.trace.enricher;
 
 public class StructuredTraceEnricherConstants {
-  public static final String SCHEMA_REGISTRY_CONFIG_KEY = "schema.registry.config";
   public static final String KAFKA_STREAMS_CONFIG_KEY = "kafka.streams.config";
-  public static final String JOB_CONFIG = "jobconfig";
   public static final String INPUT_TOPIC_CONFIG_KEY = "input.topic";
   public static final String OUTPUT_TOPIC_CONFIG_KEY = "output.topic";
   public static final String ENRICHER_CONFIGS_KEY = "enricher.configs";
   public static final String ENRICHER_NAMES_CONFIG_KEY = "enricher.names";
   public static final String ENRICHER_CONFIG_TEMPLATE = "enricher.%s";
+  public static final String STRUCTURED_TRACES_ENRICHMENT_JOB_CONFIG = "structured-traces-enrichment-job";
+
 }
